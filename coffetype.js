081ebebd10f91018d7e625d80fa9fd160cc7828c
Erblin Gashi <eg52040@ubt-uni.net>
2023-07-12 23:38:52 +0200

    some-pics-added

--- coffetype.js
+++ coffetype.js
@@ -3,14 +3,62 @@ function showImage(coffee) {
     var overlay = document.getElementById("image-overlay");
 
     switch (coffee) {
-        case "IMG_4323":
-            image.src = "IMG_4323.png";
+        case "espresso":
+            image.src = "espresso.png";
             break;
-        case "IMG_4323":
-            image.src = "IMG_4323.png";
+        case "double-espresso":
+            image.src = "double-espresso.png";
             break;
-        case "IMG_4323":
-            image.src = "IMG_4323.png";
+        case "macchiato":
+            image.src = "macchiato.png";
+            break;
+        case "small-macchiato":
+            image.src = "small-macchiato.png";
+            break;
+        case "americano":
+            image.src = "americano.png";
+            break;
+        case "ness-classic":
+            image.src = "ness-classic.png";
+            break;
+        case "ness-vanila":
+            image.src = "ness-vanila.png";
+            break;
+        case "tea":
+            image.src = "tea.png";
+            break;
+        case "latte-macchiato":
+            image.src = "latte-macchiato.png";
+            break;
+        case "hot-chocolate":
+            image.src = "hot-chocolate.png";
+            break;
+        case "uje-natyral":
+            image.src = "uje-natyral.png";
+            break;
+        case "uje-mineral":
+            image.src = "uje-mineral.png";
+            break;
+        case "coca-cola":
+            image.src = "coca-cola.png";
+            break;
+        case "americano":
+            image.src = "americano.png";
+            break;
+        case "ness-classic":
+            image.src = "ness-classic.png";
+            break;
+        case "ness-vanila":
+            image.src = "ness-vanila.png";
+            break;
+        case "tea":
+            image.src = "tea.png";
+            break;
+        case "latte-macchiato":
+            image.src = "latte-macchiato.png";
+            break;
+        case "latte-macchiato":
+            image.src = "latte-macchiato.png";
             break;
         default:
             image.src = "";
